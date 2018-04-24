9bfad1385cd99552f2b9e5e20bbb37eed1114414
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-04-24 18:24:56 -0500

    up and going

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+
+
+
+
+var Camera = require("react-native-camera");
+
+
+  React.render(
+    <Camera>
+    </Camera>
+  );
+
+
+
+// React.render(
+//   <div>
+//     <a href="#" className="button">Take a Picture</a>
+//     <div>{ipsumText}</div>
+//   </div>,
+//   document.getElementById('impl')
+// );
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Hello, Aaron</Text>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: { 
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
