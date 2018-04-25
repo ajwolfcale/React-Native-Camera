6706ff10bddd366957fb861f68b33f45dc55c796
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-04-24 21:40:06 -0500

    launching camera roll and camera

--- App.js
+++ App.js
@@ -1,42 +1,111 @@
-import React from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, Button } from 'react-native';
+import Expo from 'expo';
 
+export default class App extends Component {
+  state = {
+    chosenImage: null,
+  };
 
+  _launchCameraRollAsync = async () => {
+    let { status } = await Expo.Permissions.askAsync(
+      Expo.Permissions.CAMERA_ROLL
+    );
+    if (status !== 'granted') {
+      console.error('Camera roll permission not granted');
+      return;
+    }
 
+    let img = await Expo.ImagePicker.launchImageLibraryAsync();
+    // logs the uri of the chosen image
+    // console.log(img);
+    this.setState({ chosenImage: img });
+  };
 
-var Camera = require("react-native-camera");
+  _launchCameraAsync = async () => {
+    let { status } = await Expo.Permissions.askAsync(Expo.Permissions.CAMERA);
+    if (status !== 'granted') {
+      console.error('Camera permission not granted');
+      return;
+    }
 
+    let img = await Expo.ImagePicker.launchCameraAsync({ allowsEditing: true });
+    this.setState({ takenImage: img });
 
-  React.render(
-    <Camera>
-    </Camera>
-  );
+  // -----------use this to flip the take image horizontally and/or vertically...
+  //   let flippedImage = await Expo.ImageManipulator.manipulate(img.uri, [
+  //     {flip: {vertical: false, horizontal: false}}]);
+  //   this.setState({ takenImage: flippedImage });
+  };
 
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.paragraph}>
+          Photos
+        </Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Image
+            source={require('./aaron.jpg')}
+            style={{ height: 200, width: 200 }}
+          />
+          <Image
+            source={require('./coffee.jpg')}
+            style={{ height: 200, width: 200 }}
+          />
+        </View>
 
+        <Button
+          title="Launch Camera Roll"
+          onPress={() => {
+            this._launchCameraRollAsync();
+          }}
+        />
+        <Button
+          title="Launch Camera"
+          onPress={() => {
+            this._launchCameraAsync();
+          }}
+        />
 
-// React.render(
-//   <div>
-//     <a href="#" className="button">Take a Picture</a>
-//     <div>{ipsumText}</div>
-//   </div>,
-//   document.getElementById('impl')
-// );
+        {(this.state.chosenImage &&
+          <Image
+            source={{ uri: this.state.chosenImage.uri }}
+            style={{
+              height: 200,
+              width: 200,
+            }}
+          />) ||
+          null}
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Hello, Aaron</Text>
-//       </View>
-//     );
-//   }
-// }
+        {(this.state.takenImage &&
+          <Image
+            source={{ uri: this.state.takenImage.uri }}
+            style={{
+              height: 200,
+              width: 200,
+            }}
+          />) ||
+          null}
+        }
+      </View>
+    );
+  }
+}
 
-// const styles = StyleSheet.create({
-//   container: { 
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: Expo.Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+});
